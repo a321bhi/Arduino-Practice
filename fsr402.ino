a3cedb1260ef857e3988cce70891c799ca81c986
Abhijit <a321bhi@gmail.com>
2017-02-18 07:42:12 +0530

    Add files via upload

---
+++ fsr402.ino
@@ -0,0 +1,14 @@
+int FSR_Pin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int FSRReading = analogRead(FSR_Pin);
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+  Serial.println("Reading is: ");
+  Serial.println(FSRReading);
+  delay(250);
+}
